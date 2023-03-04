bbafeccd3db49489c72a3ddd4a5cdbe22c367405
Adaeze-Tochukwu <adarose.tochukwu@gmail.com>
2023-03-04 13:39:42 +0100

    added container

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Home.scss";
+import Container from "../components/container/Container";
+
+export default function Home() {
+  return (
+  <Container>
+
+  </Container>
+  );
+}
